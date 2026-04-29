198c9808c73e7dda9eeb8fd91e4177c4c4a53e6e
Igna <ignacioarroyo2588@gmail.com>
2026-04-29 14:26:56 +0200

    cambiados rolles

--- Bases_de_Datos/schema.sql
+++ Bases_de_Datos/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE miembro (
     id_miembro INT AUTO_INCREMENT PRIMARY KEY,
     nombre     VARCHAR(50) NOT NULL,
     apellido   VARCHAR(50) NOT NULL,
-    rol        ENUM('padre','madre','hijo','hija','otro') NOT NULL,
+    rol        VARCHAR(50) NOT NULL,
     email      VARCHAR(100) UNIQUE
 );
 
